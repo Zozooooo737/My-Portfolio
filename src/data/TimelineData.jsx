486b5d4a5a32352f5a9041ec525cc4bc999d4076
Zozooooo737 <enzo.bottura137@gmail.com>
2026-01-06 16:51:40 +0100

    Version 21 : Séparation de la data au niveau de la Timeline + Ajout du projet Tickup sur la Timeline

---
+++ src/data/TimelineData.jsx
@@ -0,0 +1,106 @@
+const timelineData = [
+  {
+    id: 1,
+    title: "Soft Skills : Améliorez Son Efficacité Individuelle et Collective",
+    date: "Novembre - 2022",
+    description:
+      "Formation sur la gestion du temps, l'apprentissage efficace, la pensée critique, la gestion de projet et le travail en équipe.",
+  },
+
+  {
+    id: 2,
+    title: "Vie Associative : Afterwork à Nantes",
+    date: "2022 - Décembre",
+    description:
+      "Organisation et encadrement d'un afterwork pour les étudiants et l'équipe pédagogique du nouveau campus nantais de l'ESILV.",
+  },
+  {
+    id: 3,
+    title: "Hackaton : Solution RSE",
+    date: "Avril - 2023",
+    description:
+      "Hackathon sur le sport durable et responsable, visant à développer le travail en équipe pluridisciplinaire et à concevoir des solutions innovantes pour des enjeux sociétaux et environnementaux.",
+  },
+  {
+    id: 4,
+    title: "Vie Associative : Secrétaire 3V",
+    date: "2023 - Avril",
+    description:
+      "Mandat en tant que secrétaire au bureau de Veni Vidi deVinci (BDE de l'ESILV).",
+  },
+  {
+    id: 5,
+    title: "Vie Associative : Intégration Etudiant ESILV",
+    date: "Septembre - 2023",
+    description:
+      "Intégration des nouveaux étudiants ingénieurs via une journée de bienvenue, une intervention en amphithéâtre, un parrainage, l'écriture d'un guide de survie et un afterwork sur Paris.",
+  },
+  {
+    id: 6,
+    title: "Vie Associative : Yellow 5",
+    date: "2023 - Octobre",
+    description:
+      "Organisation et encadrement d'une Pool Party à l'Aquaboulevard rassemblant près de 2000 personnes.",
+  },
+  {
+    id: 7,
+    title: "Projet : Scorpix - PIX 1",
+    date: "Juin - 2024",
+    description:
+      "Réalisation d'un hexapode mécanique modulable se déplaçant de façon rectiligne.",
+    link: "/projects/scorpix",
+  },
+  {
+    id: 8,
+    title: "Soft Skills : Design Thinking et Créativité",
+    date: "2024 - Septembre",
+    description:
+      "Formation sur la méthode d'innovation, créativité et résolution de problème du Design Thinking. Réalisation d'un projet en faveur de l'alimentation pour tous et le Dévéloppement durable.",
+  },
+  {
+    id: 9,
+    title: "Soft Skills : Coopérer en équipe",
+    date: "Novembre - 2024",
+    description:
+      "Formation sur les dynamiques d'équipe, les profils comportementaux, la communication positive, la gestion des conflits et le feedback constructif.",
+  },
+  {
+    id: 10,
+    title: "Projet : Boogle Zoa",
+    date: "2024 - Décembre",
+    description:
+      "Dévéloppement du jeu Boogle en C# sur console, programmation orienté objet et tests unitaires.",
+    link: "/projects/boogle-zoa",
+  },
+  {
+    id: 10,
+    title: "Hackaton : Hyperconnexion et Hypertransparence des données",
+    date: "Février - 2025",
+    description:
+      "Dévéloppement en équipe d'une solution pour la protection de notre vie privée et de nos données personnelles.",
+  },
+  {
+    id: 11,
+    title: "Soft Skills : Intelligence Collective",
+    date: "2025 - Mai",
+    description:
+      "Nous avons exploré des outils collaboratifs pour résoudre des problèmes et prendre des décisions en groupe, en tirant parti de l’intelligence collective.",
+  },
+  {
+    id: 12,
+    title: "Stage Ingénieur – Hive Robotics",
+    date: "Juin - 2025",
+    description:
+      "Stage de 2 mois chez Hive Robotics, startup développant un robot autonome de transport urbain. J’ai participé à la conception d’une nouvelle version du coffre, plus robuste et adaptée à la production en série, en réalisant diverses tâches mêlant mécatronique, impression 3D, prototypage et tests électroniques.",
+  },
+  {
+    id: 13,
+    title: "Projet : Tick'Up",
+    date: "2025 - Décembre",
+    description:
+      "Application web de réservation de séances de cinéma développée avec Symfony en architecture MVC, incluant la conception de la base de données, la logique métier, les contrôleurs et les interfaces utilisateur.",
+    link: "/projects/tick-up",
+  },
+];
+
+export default timelineData;
